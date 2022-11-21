a8eedcce4afab2f24835553fdb82c52b2382d29d
Lucas Yule <lyrma@cin.ufpe.br>
2022-11-21 17:55:49 -0300

    tests

---
+++ src/__tests__/users/model/User.spec.ts
@@ -0,0 +1,25 @@
+import { User } from './../../../modules/users/entities/User';
+import { validate } from "uuid";
+
+
+describe("User model", () => {
+  it("should be able to create an user with all props", () => {
+    const user = new User();
+
+    Object.assign(user, {
+      name: "Lucas Yule",
+      email: "lucasyule@fromspace.com",
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    expect(user).toMatchObject({
+      name: "Lucas Yule",
+      email: "lucasyule@fromspace.com",
+      admin: false,
+    });
+    expect(validate(user.id)).toBe(true);
+    expect(user.created_at).toBeInstanceOf(Date);
+    expect(user.updated_at).toBeInstanceOf(Date);
+  });
+});
